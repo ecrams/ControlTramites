22b303a82d9f5da9241d68feeda6f8b8b04308d0
marce <ecramsmcm@gmail.com>
2019-03-03 21:42:11 -0400

    adiciona compnentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,44 @@
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MatSelectModule, MatIconModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RecepcionComponent } from './componentes/recepcion/recepcion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RecepcionComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MatInputModule, 
+    MatButtonModule,
+    MatSelectModule,
+    MatIconModule,    
+    MatToolbarModule,
+    MatCardModule, 
+    MatFormFieldModule,
+
     AppRoutingModule
   ],
   providers: [],
