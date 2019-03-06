8046e1d4c17ffe283f99b464f0d5dc3c9a4df645
marce <ecramsmcm@gmail.com>
2019-03-05 23:51:25 -0400

    adicionar iconos

--- src/app/componentes/revision/revision.component.ts
+++ src/app/componentes/revision/revision.component.ts
@@ -15,7 +15,7 @@ export class RevisionComponent implements OnInit {
 
 
 
-  displayedColumns: string[] = ['nro', 'nroTramite', 'nroInmueble', 'tramite','fechaInicio','etapaTramite','observaciones'];
+  displayedColumns: string[] = ['nro', 'nroTramite', 'nroInmueble', 'tramite','fechaInicio','etapaTramite','observaciones', 'acciones'];
   //dataSource = ELEMENT_DATA;
   dataSource = new MatTableDataSource<TramitePendiente>(ELEMENT_DATA);
   @ViewChild(MatPaginator) paginator: MatPaginator;
@@ -53,15 +53,15 @@ export interface TramitePendiente {
 
 const ELEMENT_DATA: TramitePendiente[] = [
   {nro: '1', nroTramite: '123456', nroInmueble: '1245',tramite: 'ANULACION DE ACCESORIOS LAM',fechaInicio: '25/09/2018 07:39:27 a.m.',etapaTramite:'ACTIVO',observaciones:'ninguna'},
-  {nro: '2', nroTramite: '123464', nroInmueble: '11154',tramite: 'ANULACION DE ACCESORIOS LAM',fechaInicio: '25/09/2018 07:39:27 a.m.',etapaTramite:'ACTIVO',observaciones:'ninguna'},
-  {nro: '3', nroTramite: '123358', nroInmueble: '10325',tramite: 'ANULACION DE ACCESORIOS LAM',fechaInicio: '25/09/2018 07:39:27 a.m.',etapaTramite:'ACTIVO',observaciones:'ninguna'},
-  {nro: '4', nroTramite: '123499', nroInmueble: '16987',tramite: 'ANULACION DE ACCESORIOS LAM',fechaInicio: '25/09/2018 07:39:27 a.m.',etapaTramite:'ACTIVO',observaciones:'ninguna'},
-  {nro: '5', nroTramite: '123488', nroInmueble: '3698',tramite: 'ANULACION DE ACCESORIOS LAM',fechaInicio: '25/09/2018 07:39:27 a.m.',etapaTramite:'ACTIVO',observaciones:'ninguna'},
-  {nro: '6', nroTramite: '123445', nroInmueble: '4587',tramite: 'ANULACION DE ACCESORIOS LAM',fechaInicio: '25/09/2018 07:39:27 a.m.',etapaTramite:'ACTIVO',observaciones:'ninguna'},
-  {nro: '7', nroTramite: '123448', nroInmueble: '100112',tramite: 'ANULACION DE ACCESORIOS LAM',fechaInicio: '25/09/2018 07:39:27 a.m.',etapaTramite:'ACTIVO',observaciones:'ninguna'},
-  {nro: '8', nroTramite: '123447', nroInmueble: '101547',tramite: 'ANULACION DE ACCESORIOS LAM',fechaInicio: '25/09/2018 07:39:27 a.m.',etapaTramite:'ACTIVO',observaciones:'ninguna'},
-  {nro: '9', nroTramite: '123499', nroInmueble: '101472',tramite: 'ANULACION DE ACCESORIOS LAM',fechaInicio: '25/09/2018 07:39:27 a.m.',etapaTramite:'ACTIVO',observaciones:'ninguna'},
-  {nro: '10', nroTramite: '123900', nroInmueble: '22548',tramite: 'ANULACION DE ACCESORIOS LAM',fechaInicio: '25/09/2018 07:39:27 a.m.',etapaTramite:'ACTIVO',observaciones:'ninguna'},
+  {nro: '2', nroTramite: '123464', nroInmueble: '11154',tramite: 'REGISTRO INMUEBLE',fechaInicio: '25/10/2018 09:39:27 a.m.',etapaTramite:'ACTIVO',observaciones:'ninguna'},
+  {nro: '3', nroTramite: '123358', nroInmueble: '10325',tramite: 'ANULACION PLAN DE CUOTAS',fechaInicio: '25/09/2018 10:39:27 a.m.',etapaTramite:'ACTIVO',observaciones:'ninguna'},
+  {nro: '4', nroTramite: '123499', nroInmueble: '16987',tramite: 'SUSCRIPCION PLAN CUOTAS',fechaInicio: '25/09/2018 11:39:27 a.m.',etapaTramite:'ACTIVO',observaciones:'ninguna'},
+  {nro: '5', nroTramite: '123488', nroInmueble: '3698',tramite: 'ANULACION DE ACCESORIOS LAM',fechaInicio: '25/09/2018 11:39:27 a.m.',etapaTramite:'ACTIVO',observaciones:'ninguna'},
+  {nro: '6', nroTramite: '123445', nroInmueble: '4587',tramite: 'ANULACION DE ACCESORIOS LAM',fechaInicio: '25/09/2018 01:39:27 P.m.',etapaTramite:'ACTIVO',observaciones:'ninguna'},
+  {nro: '7', nroTramite: '123448', nroInmueble: '100112',tramite: 'ANULACION DE ACCESORIOS LAM',fechaInicio: '25/09/2018 07:39:27 P.m.',etapaTramite:'ACTIVO',observaciones:'ninguna'},
+  {nro: '8', nroTramite: '123447', nroInmueble: '101547',tramite: 'ANULACION PLAN DE CUOTAS',fechaInicio: '25/09/2018 07:39:27 P.m.',etapaTramite:'ACTIVO',observaciones:'ninguna'},
+  {nro: '9', nroTramite: '123499', nroInmueble: '101472',tramite: 'SUSCRIPCION PLAN CUOTAS',fechaInicio: '25/09/2018 07:39:27 P.m.',etapaTramite:'ACTIVO',observaciones:'ninguna'},
+  {nro: '10', nroTramite: '123900', nroInmueble: '22548',tramite: 'SUSCRIPCION PLAN CUOTAS',fechaInicio: '25/09/2018 07:39:27 P.m.',etapaTramite:'ACTIVO',observaciones:'ninguna'},
  
 ];
 
